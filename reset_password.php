f85b42d51961b22f1394489092a4c7e0a5acfb6f
Anish <anishanne1@gmail.com>
2019-03-17 13:33:49 -0400

    Added log recording for when users resets their password

--- reset_password.php
+++ reset_password.php
@@ -50,7 +50,16 @@
 
             mysqli_stmt_close($stmt);
         }
+        $sql = "INSERT INTO log (user, action) VALUES (?,?)";
+        if ($stmt = mysqli_prepare($db, $sql)) {
+            mysqli_stmt_bind_param($stmt, "ss", $p_user,$p_log);
+            $p_user=$_SESSION["username"];
+            $p_log="Reset password that on ".date("Y/m/d")." at ".date("h:i:s");
+            if (!mysqli_stmt_execute($stmt)) {
+                echo "oops: " . mysqli_stmt_error($stmt);
+            }
 
+        }
         mysqli_close($db);
     }
 ?>
