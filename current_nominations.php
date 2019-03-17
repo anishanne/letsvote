2da7c80fd6fc430ff15480ef2a8d6757514eedd6
Anish <anishanne1@gmail.com>
2019-03-17 16:59:52 -0400

    IMPORANT,YOU CAN DO CERTIN THINGS W/O LOGGING IN MUST FIX.

---
+++ current_nominations.php
@@ -0,0 +1,42 @@
+<?php
+require_once "mysql_config.php";
+
+$result = mysqli_query($db,"SELECT * FROM 	candidates");
+
+echo "<table border='1'>
+<tr>
+<th>Voting Code</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+    echo "<tr>";
+    echo "<td>" . $row['user'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($db);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body background="background.jpeg">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<div class="page-header">
+</div>
+<a href="/home.php" class="btn btn-primary">Back to Admin Dashboard</a>        <a href="/nominate.php" class="btn btn-primary">Back to Add a Nomination</a>
+</body>
+
+</html>
--- nominate.php
+++ nominate.php
@@ -1,1 +1,86 @@
+<?php
+function random_str()
+{
+    $keyspace = 'abcdefghijklmnopqrstuvwxyz';
+    $pieces = [];
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < 32; ++$i) {
+        $pieces [] = $keyspace[random_int(0, $max)];
+    }
+    return implode('', $pieces);
+}
 
+?>
+
+<?php
+session_start();
+
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
+    header("location: home.php");
+    exit;
+}
+
+require_once "mysql_config.php";
+
+$code = random_str();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $sql = "INSERT INTO candidates (user) VALUES (?)";
+
+    if ($stmt = mysqli_prepare($db, $sql)) {
+        mysqli_stmt_bind_param($stmt, "s", $param_code);
+
+        $param_code = $code;
+
+        if (!mysqli_stmt_execute($stmt)) {
+            echo "oops: " . mysqli_stmt_error($stmt);
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+    $sql = "INSERT INTO log (user, action) VALUES (?,?)";
+    if ($stmt = mysqli_prepare($db, $sql)) {
+        mysqli_stmt_bind_param($stmt, "ss", $p_user,$p_log);
+        $p_user=$_SESSION["username"];
+        $p_log="Added nomination for ".$code." at ".date("Y/m/d")." at ".date("h:i:s");
+        if (!mysqli_stmt_execute($stmt)) {
+            echo "oops: " . mysqli_stmt_error($stmt);
+        }
+
+    }
+    mysqli_close($db);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body background="background.jpeg">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<div class="page-header">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($code_err)) ? 'has-error' : ''; ?>">
+            <label>User</label>
+            <input type="text" name="code" class="form-control">
+            <span class="help-block"><?php echo $code_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Add Nomination">
+        </div>
+    </form>
+</div>
+<a href="/home.php" class="btn btn-primary">Back to Admin Dashboard</a>   <a href="/current_nominations.php" class="btn btn-primary">View Current Nominations</a>
+</body>
+
+</html>
