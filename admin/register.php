78dc6e948165d349f0799b7edb35f21a00f5d932
Anish <anishanne1@gmail.com>
2019-03-23 17:46:39 -0400

    Added a system to disable/enable voting w/o commenting everythin out.

--- admin/register.php
+++ admin/register.php
@@ -16,87 +16,94 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 
-<!--<?php/*
-    $username = $password = $confirm_password = "";
-    $username_err = $password_err = $confirm_password_err = "";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty(trim($_POST["username"]))) {
-            $username_err = "Please enter a username.";
-        } else {
-            $sql = "SELECT id FROM users WHERE username = ?";
+<!--To disable registration,  change the activeRegistration variable value to False-->
+<?php
+    $activeRegistration = "True";
+    if ($activeRegistration == "True") {
+        $username = $password = $confirm_password = "";
+        $username_err = $password_err = $confirm_password_err = "";
 
-            if ($stmt = mysqli_prepare($db, $sql)) {
-                mysqli_stmt_bind_param($stmt, "s", $param_username);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty(trim($_POST["username"]))) {
+                $username_err = "Please enter a username.";
+            } else {
+                $sql = "SELECT id FROM users WHERE username = ?";
 
-                $param_username = trim($_POST["username"]);
+                if ($stmt = mysqli_prepare($db, $sql)) {
+                    mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-                if (mysqli_stmt_execute($stmt)) {
-                    mysqli_stmt_store_result($stmt);
+                    $param_username = trim($_POST["username"]);
 
-                    if (mysqli_stmt_num_rows($stmt) == 1) {
-                        $username_err = "This username is already taken.";
+                    if (mysqli_stmt_execute($stmt)) {
+                        mysqli_stmt_store_result($stmt);
+
+                        if (mysqli_stmt_num_rows($stmt) == 1) {
+                            $username_err = "This username is already taken.";
+                        } else {
+                            $username = trim($_POST["username"]);
+                        }
                     } else {
-                        $username = trim($_POST["username"]);
+                        echo "Oops! Something went wrong. Please try again later.";
                     }
-                } else {
-                    echo "Oops! Something went wrong. Please try again later.";
+                }
+
+                mysqli_stmt_close($stmt);
+            }
+
+            if (empty(trim($_POST["password"]))) {
+                $password_err = "Please enter a password.";
+            } elseif (strlen(trim($_POST["password"])) < 8) {
+                $password_err = "Password must have at least 8 characters.";
+            } else {
+                $password = trim($_POST["password"]);
+            }
+
+            if (empty(trim($_POST["confirm_password"]))) {
+                $confirm_password_err = "Please confirm password.";
+            } else {
+                $confirm_password = trim($_POST["confirm_password"]);
+                if (empty($password_err) && ($password != $confirm_password)) {
+                    $confirm_password_err = "Password did not match.";
                 }
             }
 
-            mysqli_stmt_close($stmt);
-        }
+            if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+                $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 
-        if (empty(trim($_POST["password"]))) {
-            $password_err = "Please enter a password.";
-        } elseif (strlen(trim($_POST["password"])) < 8) {
-            $password_err = "Password must have at least 8 characters.";
-        } else {
-            $password = trim($_POST["password"]);
-        }
+                if ($stmt = mysqli_prepare($db, $sql)) {
+                    mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 
-        if (empty(trim($_POST["confirm_password"]))) {
-            $confirm_password_err = "Please confirm password.";
-        } else {
-            $confirm_password = trim($_POST["confirm_password"]);
-            if (empty($password_err) && ($password != $confirm_password)) {
-                $confirm_password_err = "Password did not match.";
-            }
-        }
+                    $param_username = $username;
+                    $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
-        if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
-            $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+                    if (mysqli_stmt_execute($stmt)) {
+                        header("location: login.php");
+                    } else {
+                        //echo "Something went wrong: " . mysqli_stmt_error($stmt);
+                        echo "Something went wrong";
+                    }
+                }
 
-            if ($stmt = mysqli_prepare($db, $sql)) {
-                mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+                mysqli_stmt_close($stmt);
+                $sql = "INSERT INTO log (user, action) VALUES (?,?)";
+                if ($stmt = mysqli_prepare($db, $sql)) {
+                    mysqli_stmt_bind_param($stmt, "ss", $p_user, $p_log);
+                    $p_user = $username;
+                    $p_log = "Created user account on " . date("Y/m/d") . " at " . date("h:i:s");
+                    if (!mysqli_stmt_execute($stmt)) {
+                        echo "oops: " . mysqli_stmt_error($stmt);
+                    }
 
-                $param_username = $username;
-                $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-
-                if (mysqli_stmt_execute($stmt)) {
-                    header("location: login.php");
-                } else {
-                    //echo "Something went wrong: " . mysqli_stmt_error($stmt);
-                    echo "Something went wrong";
                 }
             }
 
-            mysqli_stmt_close($stmt);
-            $sql = "INSERT INTO log (user, action) VALUES (?,?)";
-            if ($stmt = mysqli_prepare($db, $sql)) {
-                mysqli_stmt_bind_param($stmt, "ss", $p_user, $p_log);
-                $p_user = $username;
-                $p_log = "Created user account on " . date("Y/m/d") . " at " . date("h:i:s");
-                if (!mysqli_stmt_execute($stmt)) {
-                    echo "oops: " . mysqli_stmt_error($stmt);
-                }
-
-            }
+            mysqli_close($db);
         }
-
-        mysqli_close($db);
-    }*/
-
+    }
+    else{
+        echo "Registration is disabled! Please ask admins to enable."
+    }
 ?>
 
 <!DOCTYPE html>
@@ -147,5 +154,5 @@
 </div>
 <a href="home.php" class="btn btn-primary">Back to Admin Dashboard</a>
 </body>
-</html>-->
+</html>
 
