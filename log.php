f7d677b8f5f48291cf0abf4836a3a74d63b8753f
Anish <anishanne1@gmail.com>
2019-04-09 20:46:44 -0400

    V 3.0.0

--- log.php
+++ log.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="/admin.css">
+    <link rel="stylesheet" type="text/css" href="admin.css">
     <style type="text/css">
         body{
             color: #54575B;
